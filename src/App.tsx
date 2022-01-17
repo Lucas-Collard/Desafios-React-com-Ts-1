107551ef596d6cb90158143a54a4ae3c587e2e80
Lucas Collard <lucas.collard@2rpnet.com>
2022-01-17 18:54:52 -0300

    criação da div de adicionar tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { useState } from 'react';
 import * as C from './App.styles';
 import  { Item } from  './types/Item';
 import {ListItem} from './components/ListItem';
+import {AddArea} from './components/AddArea';
 
 const App = () => {
   const[list, setList] = useState<Item[]>([
     { id: 1, name: 'Comprar o pão na padaria', done: false },
     { id: 2, name: 'Comprar um bolo na padaria', done: false }
   ]);
+
   return (
       <C.Container>
         <C.Area>
           <C.Header>-- Lista de Tarefas do Lucão --</C.Header>
-        {/*Área de adicionar nova tarefa*/}
+        
+        <AddArea />
 
         {list.map((item, index)=>(
           <ListItem key={index} item={item} />
