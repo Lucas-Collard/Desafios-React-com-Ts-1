a5c40af2e61b96f77724f0d02b026b806bac30e1
Lucas Collard <lucas.collard@2rpnet.com>
2022-01-17 15:59:48 -0300

    primeira parte feita - visualização dos itens

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import * as C from './App.styles'
+import { useState } from 'react'; 
+import * as C from './App.styles';
+import  { Item } from  './types/Item';
+import {ListItem} from './components/ListItem';
 
 const App = () => {
+  const[list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar o pão na padaria', done: false },
+    { id: 2, name: 'Comprar um bolo na padaria', done: false }
+  ]);
   return (
       <C.Container>
         <C.Area>
-          <C.Header>
-            -- Lista de Tarefas do Lucão --
-          </C.Header>
+          <C.Header>-- Lista de Tarefas do Lucão --</C.Header>
+        {/*Área de adicionar nova tarefa*/}
+
+        {list.map((item, index)=>(
+          <ListItem key={index} item={item} />
+        ))}
+
         </C.Area>
       </C.Container>
    );
