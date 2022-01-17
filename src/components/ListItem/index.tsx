b5599d058731ba90fb5b02afbf7ad31c2259ea3e
Lucas Collard <lucas.collard@2rpnet.com>
2022-01-17 16:26:31 -0300

    2 parte de marcação de check box feita

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import * as C from './styles';
 import {Item} from '../../types/Item'
 
@@ -6,9 +7,16 @@ type Props = {
 }
 
 export const ListItem = ({ item }: Props) => {
+    const[isChecked, setIsChecked] = useState(item.done);
+
     return (
         <C.Container>
-            {item.name}
+            <input 
+            type="checkbox" 
+            checked={isChecked} 
+            onChange={e => setIsChecked(e.target.checked)}
+            />
+            <label>{item.name}</label>
         </C.Container>
     );
 }
--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,3 +1,23 @@
  import styled from 'styled-components';
 
- export const Container = styled.div ``;
+ export const Container = styled.div `
+    display: flex;
+    background-color: #20212C;
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    align-item: cennter;
+  
+    input{
+        width: 25px;
+        height: 25px;
+        margin-right: 5px;
+    }
+
+    label{
+        color: #CCC;
+    }
+  
+  
+    `;
+
