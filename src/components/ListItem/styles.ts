5045566dd36be27e57339b2d3c886da5d911a221
Lucas Collard <lucas.collard@2rpnet.com>
2022-01-17 16:38:21 -0300

    marcação do check box estilizada feita

--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,6 +1,11 @@
  import styled from 'styled-components';
 
- export const Container = styled.div `
+type ContainerProps = {
+    done: boolean;
+}
+
+ export const Container = styled.div(({ done }: ContainerProps)=>(
+    `
     display: flex;
     background-color: #20212C;
     padding: 10px;
@@ -16,8 +21,9 @@
 
     label{
         color: #CCC;
+        text-decoration: ${done ? 'line-through' : 'initial'};
     }
-  
-  
-    `;
+
+`
+ ));
 
